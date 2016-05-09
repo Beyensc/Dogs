5ec6a9fd8b7cbfb6c3c14433498df2fdd373d973
Beyensc <beyens.c@gmail.com>
2016-05-09 19:19:15 +0200

    maj

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  ini_set("display_errors", 1);
  error_reporting(-1);
  session_start();
- include_once('/wamp64/webapp/v0.12.02.09/webapp.class.php');  //Chemin de là ou se trouve le dossier webapp, généralement /var/lib/
+ include_once('/var/lib/webapp/v0.12.02.09/webapp.class.php');  //Chemin de là ou se trouve le dossier webapp, généralement /var/lib/
 // print_r($_COOKIE);
 // echo '<br />';
  //print_r($_SESSION);
